d27a45e2d52774f27c7a4a950705529971ec4783
Pablo Valiente <pabloantonio.valiente@beeva.com>
2017-02-15 23:15:11 +0100

    Added tests for TrustedAdvisorClientTest

--- src/test/java/com/beeva/trustedoverlord/clients/tests/TrustedAdvisorClientTest.java
+++ src/test/java/com/beeva/trustedoverlord/clients/tests/TrustedAdvisorClientTest.java
@@ -50,11 +50,12 @@ public class TrustedAdvisorClientTest {
             AsyncHandler<DescribeTrustedAdvisorChecksRequest, DescribeTrustedAdvisorChecksResult> handler =
                     invocation.getArgument(1);
 
-            handler.onSuccess(request, createChecks());
+            DescribeTrustedAdvisorChecksResult result = createChecks();
+            handler.onSuccess(request, result);
 
             Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
 
-            return null;
+            return CompletableFuture.completedFuture(result);
         }).when(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
 
         // Capture the parameters when invoking the method describeTrustedAdvisorCheckResultAsync (second callback)
@@ -93,6 +94,192 @@ public class TrustedAdvisorClientTest {
         verify(mockClient, never()).shutdown();
 
     }
+
+    @Test
+    public void testGetProfileChecksAutoshutdown() throws Exception {
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorChecksAsync (first callback)
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorChecksRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorChecksRequest, DescribeTrustedAdvisorChecksResult> handler =
+                    invocation.getArgument(1);
+
+            DescribeTrustedAdvisorChecksResult result = createChecks();
+            handler.onSuccess(request, result);
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(result);
+        }).when(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorCheckResultAsync (second callback)
+        DescribeTrustedAdvisorCheckResultResult describeTrustedAdvisorCheckResultResult = createChecksResult();
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorCheckResultRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorCheckResultRequest, DescribeTrustedAdvisorCheckResultResult> handler =
+                    invocation.getArgument(1);
+
+            handler.onSuccess(request, describeTrustedAdvisorCheckResultResult);
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(describeTrustedAdvisorCheckResultResult);
+        }).when(mockClient).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+
+        // Calls the method
+        TrustedAdvisorClient trustedClient = TrustedAdvisorClient.withClient(mockClient).autoshutdown();
+        CompletableFuture<ProfileChecks> future = (CompletableFuture<ProfileChecks>) trustedClient.getProfileChecks();
+
+        Assert.assertThat(trustedClient.isAutoshutdown(), is(true));
+
+        // Waits until the future is complete
+        Awaitility.await().until(future::isDone);
+
+        Assert.assertThat(future.get().getErrors().isEmpty(), is(false));
+        Assert.assertThat(future.get().getErrors().size(), is(2));
+        Assert.assertThat(future.get().getErrors().get(0), is("Name 1"));
+        Assert.assertThat(future.get().getExceptions().isEmpty(), is(true));
+        Assert.assertThat(future.get().getWarnings().isEmpty(), is(false));
+        Assert.assertThat(future.get().getWarnings().size(), is(2));
+        Assert.assertThat(future.get().getWarnings().get(1), is("Name 4"));
+
+        verify(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+        verify(mockClient, times(4)).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+        verify(mockClient).shutdown();
+
+    }
+
+
+    @Test
+    public void testGetProfileChecksEmptyChecksResult() throws Exception {
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorChecksAsync (first callback)
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorChecksRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorChecksRequest, DescribeTrustedAdvisorChecksResult> handler =
+                    invocation.getArgument(1);
+
+            handler.onSuccess(request, new DescribeTrustedAdvisorChecksResult().withChecks(Collections.emptyList()));
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(new DescribeTrustedAdvisorChecksResult().withChecks(Collections.emptyList()));
+        }).when(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+
+        // Calls the method
+        TrustedAdvisorClient trustedClient = TrustedAdvisorClient.withClient(mockClient);
+        CompletableFuture<ProfileChecks> future = (CompletableFuture<ProfileChecks>) trustedClient.getProfileChecks();
+
+        Assert.assertThat(trustedClient.isAutoshutdown(), is(false));
+
+        // Waits until the future is complete
+        Awaitility.await().until(future::isDone);
+
+        Assert.assertThat(future.get().getErrors().isEmpty(), is(true));
+        Assert.assertThat(future.get().getExceptions().isEmpty(), is(true));
+        Assert.assertThat(future.get().getWarnings().isEmpty(), is(true));
+
+        verify(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+        verify(mockClient, never()).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+        verify(mockClient, never()).shutdown();
+
+    }
+
+    @Test
+    public void testGetProfileChecksException() throws Exception {
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorChecksAsync (first callback)
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorChecksRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorChecksRequest, DescribeTrustedAdvisorChecksResult> handler =
+                    invocation.getArgument(1);
+
+            AmazonClientException testException = new AmazonClientException("testException");
+            handler.onError(testException);
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(null);
+        }).when(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+
+        // Calls the method
+        TrustedAdvisorClient trustedClient = TrustedAdvisorClient.withClient(mockClient);
+        CompletableFuture<ProfileChecks> future = (CompletableFuture<ProfileChecks>) trustedClient.getProfileChecks();
+
+        Assert.assertThat(trustedClient.isAutoshutdown(), is(false));
+
+        // Waits until the future is complete
+        Awaitility.await().until(future::isDone);
+
+        try{
+            future.get(); //This should throw an exception
+            Assert.fail();
+        }
+        catch (Exception e){
+            Assert.assertThat(e, instanceOf(ExecutionException.class));
+            Assert.assertThat(e.getCause(), instanceOf(AmazonClientException.class));
+        }
+
+        verify(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+        verify(mockClient, never()).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+        verify(mockClient, never()).shutdown();
+
+    }
+
+    /**
+     * Verifies the behaviour when the inner invocation method (describeTrustedAdvisorCheckResultAsync) returns exceptions
+     */
+    @Test
+    public void testGetProfileChecksInnerException() throws Exception {
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorChecksAsync (first callback)
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorChecksRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorChecksRequest, DescribeTrustedAdvisorChecksResult> handler =
+                    invocation.getArgument(1);
+
+            DescribeTrustedAdvisorChecksResult result = createChecks();
+            handler.onSuccess(request, result);
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(result);
+        }).when(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+
+        // Capture the parameters when invoking the method describeTrustedAdvisorCheckResultAsync (second callback)
+        doAnswer(invocation -> {
+            DescribeTrustedAdvisorCheckResultRequest request = invocation.getArgument(0);
+            AsyncHandler<DescribeTrustedAdvisorCheckResultRequest, DescribeTrustedAdvisorCheckResultResult> handler =
+                    invocation.getArgument(1);
+
+            AmazonClientException testException = new AmazonClientException("testException");
+            handler.onError(testException);
+
+            Assert.assertThat(request.getLanguage(), is(Locale.ENGLISH.getLanguage()));
+
+            return CompletableFuture.completedFuture(null);
+        }).when(mockClient).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+
+        // Calls the method
+        TrustedAdvisorClient trustedClient = TrustedAdvisorClient.withClient(mockClient);
+        CompletableFuture<ProfileChecks> future = (CompletableFuture<ProfileChecks>) trustedClient.getProfileChecks();
+
+        Assert.assertThat(trustedClient.isAutoshutdown(), is(false));
+
+        // Waits until the future is complete
+        Awaitility.await().until(future::isDone);
+
+        Assert.assertThat(future.get().getErrors().isEmpty(), is(true));
+        Assert.assertThat(future.get().getExceptions().isEmpty(), is(false));
+        Assert.assertThat(future.get().getExceptions().size(), is(4));
+        Assert.assertThat(future.get().getWarnings().isEmpty(), is(true));
+
+        verify(mockClient).describeTrustedAdvisorChecksAsync(any(DescribeTrustedAdvisorChecksRequest.class), any());
+        verify(mockClient, times(4)).describeTrustedAdvisorCheckResultAsync(any(DescribeTrustedAdvisorCheckResultRequest.class), any());
+        verify(mockClient, never()).shutdown();
+
+    }
+
 
     private DescribeTrustedAdvisorChecksResult createChecks() {
 
